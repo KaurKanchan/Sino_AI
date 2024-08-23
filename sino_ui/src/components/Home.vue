eb5aa99a41734f0d958eb388c0f1d4e63a9db30b
Aman Thakur <161003489+gurleen124@users.noreply.github.com>
2024-08-23 13:06:04 +0530

    subscribe

--- sino_ui/src/components/Home.vue
+++ sino_ui/src/components/Home.vue
@@ -25,29 +25,6 @@
   <!-- /hero section -->
   <!-- database section -->
   <div class="text-center text-black bg-light px-15">
-    <!-- <v-container class="py-16">
-      <h1 class="text-theme-primary mb-16 font-pt-serif">China AI Database</h1>
-      <v-row>
-        <v-col
-          v-for="(message, index) in blogPosts"
-          :key="index"
-          md="4"
-          cols="12"
-          class="px-md-16 mb-6"
-        >
-          <BlogPost
-            :category="message.category"
-            :img="message.img"
-            :doc1="message.doc1"
-            :desc1="message.desc1"
-            :doc2="message.doc2"
-            :desc2="message.desc2"
-            :doc3="message.doc3"
-            :desc3="message.desc3"
-          />
-        </v-col>
-      </v-row>
-    </v-container> -->
     <v-container class="py-16">
       <h1 class="text-theme-primary mb-16 font-pt-serif">China AI Database</h1>
       <v-row>
@@ -72,7 +49,7 @@
             img="https://img.freepik.com/free-vector/market-growth-concept-business-progress-business-expansion-idea-company-promotion-new-marketplace-finance-increase-business-success-flat-vector-illustration_613284-1292.jpg"
           />
           <v-list-item
-            v-for="(item, index) in govtItems"
+            v-for="(item, index) in marketItems"
             :key="index"
             class="text-start"
           >
@@ -87,7 +64,7 @@
             img="https://t4.ftcdn.net/jpg/00/94/42/23/360_F_94422350_NLHh8XUVbSQn2GhMOxcgPinKNJKeB4JO.jpg"
           />
           <v-list-item
-            v-for="(item, index) in govtItems"
+            v-for="(item, index) in pvtItems"
             :key="index"
             class="text-start"
           >
@@ -180,10 +157,10 @@ export default {
   },
   data() {
     return {
-      items: [], // The full items array from your API
-      marketItems: [], // First 3 items in the "Market" category
-      govtItems: [], // First 3 items in the "Government" category
-      pvtItems: [], // First 3 items in the "Private" category
+      items: [], // The full items array from API
+      marketItems: [],
+      govtItems: [],
+      pvtItems: [],
       loading: false,
       error: null,
       blogPosts: [
@@ -200,46 +177,6 @@ export default {
           img: "https://t4.ftcdn.net/jpg/00/94/42/23/360_F_94422350_NLHh8XUVbSQn2GhMOxcgPinKNJKeB4JO.jpg",
         },
       ],
-      // blogPosts: [
-      //   {
-      //     category: "Government",
-      //     img: "https://etimg.etb2bimg.com/thumb/msid-106291853,imgsize-62380,width-1200,height=765,overlay-etgovernment/blog/10-govtech-trends-for-2024-major-futuristic-tech-trends-to-watch-in-government-sector-in-2024.jpg",
-      //     doc1: "Political theories and ideologies",
-      //     desc1:
-      //       "Provides a novel approach to the analysis of ideologies, through examining their internal conceptual morphology. ",
-      //     doc2: "Democracy",
-      //     desc2: "Maintaining law and order with concept of power sharing",
-      //     doc3: "Constitution",
-      //     desc3:
-      //       "Collection of fundamental principles and established precedents that define how an entity, such as a nation, state, or social group, is governed",
-      //   },
-      //   {
-      //     category: "Market",
-      //     img: "https://img.freepik.com/free-vector/market-growth-concept-business-progress-business-expansion-idea-company-promotion-new-marketplace-finance-increase-business-success-flat-vector-illustration_613284-1292.jpg",
-      //     doc1: "Consumer behavior",
-      //     desc1:
-      //       "The actions and decisions that people make when they are purchasing or using products",
-      //     doc2: "Competition ",
-      //     desc2:
-      //       "A market with many buyers and sellers, where no one individual has the ability to affect the market",
-      //     doc3: "Monopoly in market",
-      //     desc3:
-      //       "A market structure where a single company or group of companies has exclusive control over the production and distribution of a product or service",
-      //   },
-      //   {
-      //     category: "Private sector",
-      //     img: "https://t4.ftcdn.net/jpg/00/94/42/23/360_F_94422350_NLHh8XUVbSQn2GhMOxcgPinKNJKeB4JO.jpg",
-      //     doc1: "Independent management",
-      //     desc1:
-      //       "The management of the private sector is entirely dependent on the actions of its owners",
-      //     doc2: "Work culture of employees",
-      //     desc2:
-      //       "The private sector has high openness, high confrontation, high trust, high authenticity, high proaction, high autonomy, high collaboration and high experimentation.",
-      //     doc3: "Profit motive",
-      //     desc3:
-      //       "The profit motive is the drive or incentive for individuals and businesses to maximize their financial gains.",
-      //   },
-      // ],
       analysisPosts: [
         {
           subject: "Money",
--- sino_ui/src/components/Subscribe.vue
+++ sino_ui/src/components/Subscribe.vue
@@ -22,50 +22,69 @@
   </div>
   <!-- /hero section -->
   <!-- subscribe form -->
-  <div class="bg-theme-primary">
+  <div class="bg-light">
     <v-container class="py-16">
-      <v-row no-gutters class="justify-center text-center align-center">
+      <v-row no-gutters class="justify-center align-center">
         <v-col md="6">
-          <v-row class="gr-md-14">
-            <v-col md="6" class="pe-6"
-              ><v-text-field
+          <v-row class="gr-md-10">
+            <v-col md="6" class="pe-6">
+              <div
+                class="text-h6 font-weight-regular text-black font-open-sans"
+              >
+                First Name
+              </div>
+              <v-text-field
                 hide-details="auto"
                 class="input-form"
-                variant="solo"
+                variant="outlined"
+                base-color="#CBD2E0"
                 bg-color="white"
-                placeholder="First Name"
               ></v-text-field
             ></v-col>
-            <v-col md="6" class="ps-6"
-              ><v-text-field
+            <v-col md="6" class="ps-6">
+              <div
+                class="text-h6 font-weight-regular text-black font-open-sans"
+              >
+                Last Name
+              </div>
+              <v-text-field
                 class="input-form"
                 hide-details="auto"
-                variant="solo"
+                variant="outlined"
                 bg-color="white"
-                placeholder="Last Name"
-              ></v-text-field
-            ></v-col>
-            <v-col md="12"
-              ><v-text-field
-                class="input-form"
-                hide-details="auto"
-                variant="solo"
-                bg-color="white"
-                placeholder="Email"
-              ></v-text-field
-            ></v-col>
-            <v-col md="12"
-              ><v-text-field
-                class="input-form"
-                hide-details="auto"
-                variant="solo"
-                bg-color="white"
-                placeholder="Area of Interests"
+                base-color="#CBD2E0"
               ></v-text-field
             ></v-col>
             <v-col md="12">
+              <div
+                class="text-h6 font-weight-regular text-black font-open-sans"
+              >
+                Email
+              </div>
+              <v-text-field
+                class="input-form"
+                hide-details="auto"
+                variant="outlined"
+                base-color="#CBD2E0"
+                bg-color="white"
+              ></v-text-field
+            ></v-col>
+            <v-col md="12">
+              <div
+                class="text-h6 font-weight-regular text-black font-open-sans"
+              >
+                Area of Interests (optional)
+              </div>
+              <v-text-field
+                class="input-form"
+                hide-details="auto"
+                variant="outlined"
+                base-color="#CBD2E0"
+                bg-color="white"
+              ></v-text-field
+            ></v-col>
+            <v-col md="12" class="text-black">
               <v-checkbox
-                color="white"
                 class="input-form"
                 label="I agree to receive emails from SinoAI Advisory."
               ></v-checkbox>
